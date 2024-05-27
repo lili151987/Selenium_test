75d202273c47f070442bfdb20408099b3828dd0a
Lilian Garcia <lilianaraceligportal@gmail.com>
2024-05-27 16:39:49 -0600

    "subiendo"

--- src/test/java/prueba.java
+++ src/test/java/prueba.java
@@ -1,24 +1,65 @@
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class prueba {
 
     @Test
-    public void test1() {
+    public void testWaits() throws InterruptedException {
 
-        ChromeDriver browser = new ChromeDriver();
+            ChromeDriver browser = new ChromeDriver();
 
-        browser.get("https://www.google.com");
+            browser.get("https://www.saucedemo.com/");
 
-        WebElement search = browser.findElement(By.name("q"));
+            //browser.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+            WebDriverWait wait = new WebDriverWait(browser,Duration.ofSeconds(15));
 
-        search.sendKeys("Que es Java");
-        search.submit();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("user-name")));
+            WebElement userName = browser.findElement(By.name("user-name"));
+            WebElement password = browser.findElement(By.name("password"));
+            WebElement login = browser.findElement(By.id("login-button"));
+
+            userName.sendKeys("standard_user");
+            password.sendKeys("secret_sauce");
+            login.click();
 
 
 
+
+        }
+
+        @Test
+        public void TestLoginFail(){
+            ChromeDriver browser = new ChromeDriver();
+
+            browser.get("https://www.saucedemo.com/");
+
+
+            WebDriverWait wait = new WebDriverWait(browser,Duration.ofSeconds(15));
+
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("user-name")));
+
+            WebElement userName = browser.findElement(By.name("user-name"));
+            WebElement password = browser.findElement(By.name("password"));
+            WebElement login = browser.findElement(By.id("login-button"));
+
+            userName.sendKeys("standard_user11");
+            password.sendKeys("secret_sauce");
+
+            login.click();
+
+            WebElement errorMessage = browser.findElement(By.xpath("//h3[@data-test='error']"));
+
+            Assert.assertEquals("Epic sadface: Username and password do not match any user in this service",errorMessage.getText());
+
+            login.click();
+        }
     }
 
-}
